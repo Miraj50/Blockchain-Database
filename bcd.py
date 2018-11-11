8c4d5f2efebfca954cca85155b422c0e0a0b89c0
Rishabh Raj <rishabhstpaul@gmail.com>
2018-11-11 05:54:54 +0530

    Treeview instead of Listbox

--- bcd.py
+++ bcd.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import tkinter.messagebox as msgbox
 from tkinter import scrolledtext, simpledialog
-import tkinter.ttk
+import tkinter.ttk as ttk
 import requests
 import os, hashlib
 from Crypto.PublicKey import RSA
@@ -13,7 +13,7 @@ class BcD(tk.Tk):
 		self.uname = ""
 		self.eG = None
 		self.vG = None
-		self.start()
+		self.Home()
 
 	def start(self):
 		self.sess = requests.Session()
@@ -40,7 +40,7 @@ class BcD(tk.Tk):
 		loginButton.grid(columnspan=2, row=4, column=1, pady=(5,10))
 		# loginButton.bind('<Return>', lambda e: CheckLogin(nameBox.get(), pwordBox.get()))
 
-		tkinter.ttk.Separator(self, orient="horizontal").grid(column=1, row=5, columnspan=3, sticky='nsew')
+		ttk.Separator(self, orient="horizontal").grid(column=1, row=5, columnspan=3, sticky='nsew')
 
 		#SignUp
 		signUpText = tk.Label(self, text='Sign Up', font='Helvetica 16 bold', fg='brown')
@@ -136,7 +136,7 @@ class BcD(tk.Tk):
 
 	def Home(self):
 		self.clear_widgets()
-		# self.attributes('-zoomed', True)
+		self.attributes('-zoomed', True)
 		self.title('Grades')
 		self.grid_rowconfigure(2, weight=1)
 		self.grid_columnconfigure(1, weight=1)
@@ -155,71 +155,83 @@ class BcD(tk.Tk):
 		logoutButton = tk.Button(self, text='LogOut', bg='brown', fg='white', command=self.Logout)
 		logoutButton.grid(row=0, column=1, padx=(0,10), pady=(5,5), sticky="e")
 
-		tkinter.ttk.Separator(self, orient="horizontal").grid(column=0, row=1, columnspan=2, sticky='nsew')
+		ttk.Separator(self, orient="horizontal").grid(column=0, row=1, columnspan=2, sticky='nsew')
 
 		enterButton = tk.Button(self, text='Enter Grades', bg='blue', fg='white', command=self.enterG)
 		enterButton.grid(row=1, column=0, padx=(40,5), pady=(5,2), sticky="e")
 		viewButton = tk.Button(self, text='View Grades', bg='blue', fg='white', command=self.viewG)
 		viewButton.grid(row=1, column=1, padx=(5,40), pady=(5,2), sticky="w")
 
-		tkinter.ttk.Separator(self, orient="horizontal").grid(column=0, row=2, columnspan=2, sticky='nsew')
+		ttk.Separator(self, orient="horizontal").grid(column=0, row=2, columnspan=2, sticky='nsew')
 
 	def enterG(self):
 		self.geometry("")
+		if self.vG != None:
+			self.vG.grid_forget()
 
 		if self.eG == None:
 			self.eG = tk.Frame(self)
 			# eGrade.grid_columnconfigure(0, weight=1) # Along with sticky=ewns will resize to full window
 			self.eG.grid_rowconfigure(0, weight=1)
 
-			enterGrades = scrolledtext.ScrolledText(self.eG, font='Times 11', wrap='word', spacing2=0, spacing3=7, width=64, height=10)
+			enterGrades = scrolledtext.ScrolledText(self.eG, font='Times 11', wrap='word', spacing2=0, spacing3=7, width=65, height=12)
 			enterGrades.grid(row=0, column=0, sticky="ewns")
 
 			enterGBut = tk.Button(self.eG, text='Submit Grades', bg='green', fg='white', command=lambda: self.submitG(enterGrades.get("1.0", 'end-1c')))
 			enterGBut.grid(row=1, column=0, columnspan=2, pady=(0,0), sticky="ns")
-			self.eG.grid(row=2, column=0, columnspan=2, padx=(10,10), pady=(1,5), sticky="ns")
+			self.eG.grid(row=2, column=0, columnspan=2, padx=(10,10), pady=(1,3), sticky="ns")
 		else:
-			if self.vG != None:
-				self.vG.grid_forget()
-			self.eG.grid(row=2, column=0, columnspan=2, padx=(10,10), pady=(1,5), sticky="ns")
+			self.eG.grid(row=2, column=0, columnspan=2, padx=(10,10), pady=(1,3), sticky="ns")
 
 	def viewG(self):
 		self.geometry("")
+
+		if self.eG != None:
+			self.eG.grid_forget()
 
 		if self.vG == None:
 			self.vG = tk.Frame(self)
 			# vGrade.grid_columnconfigure(0, weight=1)
 			self.vG.grid_rowconfigure(0, weight=1)
 
-			viewList = tk.Listbox(self.vG, font='Times 11', width=64, height=15, relief='sunken', justify="center")
-			viewList.insert(1, "160050029    |    Rishabh Raj    |    CS333    |    AA")
-			viewList.insert(2, "160050056    |    Kumar Saurav   |    CS333    |    AP")
-			viewList.insert(3, "160050057    |    Kumar Saunack  |    CS333    |    AP")
+			viewList = ttk.Treeview(self.vG, height=15)
+			viewList['show'] = 'headings'
+			viewList['columns'] = ('Roll', 'Name', 'Course', 'Grade')
+			viewList.heading("#1", text='Roll No.', anchor='w')
+			viewList.column("#1", stretch="no", width=100)
+			viewList.heading("#2", text='Name', anchor='w')
+			viewList.column("#2", stretch="no", width=160 )
+			viewList.heading("#3", text='Course', anchor='w')
+			viewList.column("#3", stretch="no", width=100)
+			viewList.heading("#4", text='Grade', anchor='w')
+			viewList.column("#4", stretch="no", width=100)
+
+			viewList.insert("", "end", values=("160050029", "Rishabh Raj", "CS333", "AA"))
+			viewList.insert("", "end", values=("160050057", "Kumar Saurav", "CS333", "AP"))
+			viewList.insert("", "end", values=("160050056", "Kumar Saunack", "CS333", "AP"))
 			viewList.grid(row=0, column=0, sticky="ns")
 
 			yscroll = tk.Scrollbar(self.vG, command=viewList.yview, orient="vertical")
-			yscroll.grid(row=0, column=1, sticky='nws')
+			yscroll.grid(row=0, column=1, sticky='ns')
 
 			viewList.configure(yscrollcommand=yscroll.set)
 			viewList.bind("<Double-Button-1>", self.updateG)
 
 			self.vG.grid(row=2, column=0, columnspan=2, padx=(10,10), pady=(1,7), sticky="ns")
 		else:
-			if self.eG != None:
-				self.eG.grid_forget()
 			self.vG.grid(row=2, column=0, columnspan=2, padx=(10,10), pady=(1,7), sticky="ns")
 
 	def submitG(self, grades):
 		print(grades)
 
 	def updateG(self, event):
-		idx = int(event.widget.curselection()[0])
-		value = event.widget.get(idx).split('|')
+		w = event.widget
+		idx = w.selection()[0]
+		item = w.item(w.selection())['values']
 
-		uG = simpledialog.askstring('Update grade', 'Enter New Grade', parent=self, initialvalue=value[3].strip())
-		value[3] = "    " + uG
-		self.vG.winfo_children()[0].delete(idx)
-		self.vG.winfo_children()[0].insert(idx, "|".join(value))
+		uG = simpledialog.askstring('Update grade', 'Enter New Grade', parent=self, initialvalue=item[3])
+
+		self.vG.winfo_children()[0].item(idx, values=(item[0], item[1], item[2], uG))
 
 	def Logout(self):
 		url = 'http://localhost/logout.php'
@@ -230,7 +242,6 @@ class BcD(tk.Tk):
 			msgbox.showerror('Error', 'Some Error has Occurred !')
 		else:
 			self.attributes('-zoomed', False)
-			# self.text_data = self.txt.get(1.0, "end-1c")
 			self.clear_widgets()
 			self.uname = ""
 			self.eG = None
